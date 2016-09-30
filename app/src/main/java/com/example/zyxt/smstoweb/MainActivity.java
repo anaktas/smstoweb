590a6f7580e88b3210ad2080229e8c658a86d414
Anastasios Daris <t.daris@7linternational.com>
2016-09-30 10:30:46 +0300

    Button creation and initial implementation of the receiver and response service

--- app/src/main/java/com/example/zyxt/smstoweb/MainActivity.java
+++ app/src/main/java/com/example/zyxt/smstoweb/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.zyxt.smstoweb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toggleChecked(View view) {
+        //final ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton);
+        boolean isOn = ((ToggleButton) view).isChecked();
+
+        if(isOn) {
+            //enableIncomingCallReceiver();
+        } else {
+            //disableIncomingCallReceiver();
+        }
     }
 }
